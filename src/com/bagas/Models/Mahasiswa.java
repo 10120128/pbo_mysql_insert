5ec984d044621c612d0fc35693373180c011509a
R. Bagas <87528336+10120128@users.noreply.github.com>
2022-01-16 14:39:01 +0700

    Menambahkan constructor pada class Mahasiswa

--- src/com/bagas/Models/Mahasiswa.java
+++ src/com/bagas/Models/Mahasiswa.java
@@ -5,4 +5,11 @@ public class Mahasiswa {
     private String nim;
     private String alamat;
     private int nilai;
+
+    public Mahasiswa(String nama, String nim, String alamat, int nilai) {
+        this.nama = nama;
+        this.nim = nim;
+        this.alamat = alamat;
+        this.nilai = nilai;
+    }
 }
